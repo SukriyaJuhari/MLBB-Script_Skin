96895de809d8118d6b73b00a984ad0c7964295ed
sukriyajuhari <sukriyajuhari@gmail.com>
2025-11-26 22:56:23 +0700

    update2025NOV24

---
+++ PostInjectContent.js
@@ -0,0 +1,35 @@
+export default class PostInjectContent {
+  constructor(rootData) {
+        // Inject Content
+      document.querySelector(".fileName").innerText = rootData.fileName;
+      document.querySelector(".metaName").innerHTML =
+        `${rootData.fileName} <span style="background:#ddd;padding:2px 8px;border-radius:999px;font-size:12px;">${rootData.fileType}</span>`;
+      document.querySelector(".metaDate").innerText = rootData.fileDate;
+      
+      // Generate option dari rootData
+      const select = document.querySelector(".skinSelect");
+      rootData.options.forEach(opt => {
+        const o = document.createElement("option");
+        o.value = opt.value;
+        o.textContent = opt.name;
+        if (opt.selected) o.selected = true;
+        select.appendChild(o);
+      })
+      const downloadBtn = document.querySelector(".downloadLink");
+      const metaSize = document.querySelector(".metaSize");
+      
+      
+      function updateLink() {
+        const selected = select.value;
+        const selectedOption = rootData.options.find(opt => opt.value === selected);
+        downloadBtn.href = `${rootData.baseDownloadUrl}=${rootData.nameHero}_${selected}=${rootData.skinKategor}`;
+        console.log(downloadBtn.href)
+        
+        
+        metaSize.innerText = selectedOption.size;
+      }
+      
+      select.addEventListener("change", updateLink);
+      updateLink(); // set default awal
+  }
+}
---
+++ dataFileLinks.js
@@ -0,0 +1,122 @@
+export default class dataFileLinks {
+  constructor() {
+    this.link = {
+      //################################################################################################################      
+      //####################################################################################################################
+      //Alucard Script Skin
+      //================================================================================================================================
+      //Alucard Script Skin Legend
+      "Alucard_Default=Legend": "https://drive.google.com/file/d/1butgl5dtbvCC2EyVk6sA3O_Z8H20bcke/view?usp=drivesdk",
+      "Alucard_Basic=Legend": "https://drive.google.com/file/d/1tTpmGXs7TTwoQyEopgR6Rg1e2ymuggHg/view?usp=drivesdk",
+      "Alucard_EmpireAgent=Legend": "https://drive.google.com/file/d/1t2g74EEbeObXOWfJpnKj7NXgKhCrAcVJ/view?usp=drivesdk",
+      "Alucard_Lightborn=Legend": "https://drive.google.com/file/d/1MLEuPPKLNN-iV5jLOzXjMsuSPTrEieeL/view?usp=drivesdk",
+      "Alucard_Luckybox=Legend": "https://drive.google.com/file/d/16Z21xyqN6aJ2BPo8dEP4xDfog9Vg6SnS/view?usp=drivesdk",
+      "Alucard_Season=Legend": "https://drive.google.com/file/d/1569IOR5EBnO9l9Gll4ez6t4NkBERUxvb/view?usp=drivesdk",
+      "Alucard_Starlight=Legend": "https://drive.google.com/file/d/1hE0xCw_vMa-N-CB78PF4uieJhoQy7C1l/view?usp=drivesdk",
+      "Alucard_Valentine=Legend": "https://drive.google.com/file/d/1GKOZONLxObYJbmr_Aaws6PpOHfQAHnlT/view?usp=drivesdk",
+      //================================================================================================================================
+      //Alucard Script Skin Lightborn
+      "Alucard_Basic=Lightborn": "https://drive.google.com/file/d/1rwF-AWwtCqO5ikZat2nbniz-znyxHZvy/view?usp=drivesdk",
+      "Alucard_Default=Lightborn": "https://drive.google.com/file/d/1wB3BEUqhm39bsqYvj-DgSLqZko-sYbYw/view?usp=drivesdk",
+      "Alucard_EmpireAgent=Lightborn": "https://drive.google.com/file/d/1G3FosbyLj1-eiKb8OkWgP50sNF8Av6FX/view?usp=drivesdk",
+      "Alucard_Season=Lightborn": "https://drive.google.com/file/d/1ZE01KpPOD5QrAYPv0EbffnrN47iKSnof/view?usp=drivesdk",
+      "Alucard_Starlight=Lightborn": "https://drive.google.com/file/d/1sjbL2HqPDz0GTeTNxNM0_bq0ocSoJTKk/view?usp=drivesdk",
+      //================================================================================================================================
+      //Alucard Script Skin Luckybox
+      "Alucard_Basic=Luckybox": "https://drive.google.com/file/d/14PtqC8v4NlSSUmg6L1Pf77pO3daviLCq/view?usp=drivesdk",
+      "Alucard_Default=Luckybox": "https://drive.google.com/file/d/1xXutHA1XPW2egdXR9FA9OE6rCmpemJdm/view?usp=drivesdk",
+      "Alucard_EmpireAgent=Luckybox": "https://drive.google.com/file/d/1h6NUn5D21JPdgmAVvKkXcQHH_kiq96b2/view?usp=drivesdk",
+      "Alucard_Lightborn=Luckybox": "https://drive.google.com/file/d/1nhEIrBtuX3zqyX3zRFPV0H9-VhVPBrEL/view?usp=drivesdk",
+      "Alucard_Season=Luckybox": "https://drive.google.com/file/d/1rAY0n8d1Dv_aLNudDIfDISs4SoXZuG8q/view?usp=drivesdk",
+      "Alucard_Starlight=Luckybox": "https://drive.google.com/file/d/1KAMnk_3DDXGlZzTH6kG1somuN-VOBLYG/view?usp=drivesdk",
+      "Alucard_Valentine=Luckybox": "https://drive.google.com/file/d/1Y4aMQwTbT-nlUE1oVZDJOTFBhJ8TUmwH/view?usp=drivesdk",
+      //Alucard Script Skin StarWars
+      "Alucard_Basic=StarWars": "https://drive.google.com/file/d/13Gwt8xaQd2gX8mq5itL90xD2LHs1qon2/view?usp=drivesdk",
+      "Alucard_Default=StarWars": "https://drivepl.google.com/file/d/1jKdOwvWbK2qo2Vlag2llcC-YabuGLIK7/view?usp=drivesdk",
+      "Alucard_Season=StarWars": "https://drive.google.com/file/d/1cnO9oq-8uJY0DjYjR6cbJh2xnsuwvWDQ/view?usp=drivesdk",
+      
+      //################################################################################################################      
+      //####################################################################################################################
+      
+      //Benedeta Script Skin
+      //================================================================================================================================
+      //Benedeta_Collector
+      "Benedetta_Basic=Collector": "https://drive.google.com/file/d/132BvZuG1suhshUaF0UBfhKASGvqe6Cpd/view?usp=drivesdk",
+      "Benedetta_Default=Collector": "https://drive.google.com/file/d/1RUUfEHorVDL-AhyEsAUtL-HA1NeSprQf/view?usp=drivesdk",
+      "Benedetta_Special=Collector": "https://drive.google.com/file/d/1_2wTFvbaU7-c_9GzrCvblf6ire89_26d/view?usp=drivesdk",
+      "Benedetta_Starlight=Collector": "https://drive.google.com/file/d/1NmY8uz8R6OGUCUGYrIGM1dHM9Yl0obGh/view?usp=drivesdk",
+      "Benedetta_MPL=Collector": "https://drive.google.com/file/d/1BIc14oSDxWMVWKvkDz5LvSASrYC4vjF4/view?usp=drivesdk",
+      //================================================================================================================================
+      //Benedeta_Covenant
+      "Benedetta_Default=Covenant": "https://drive.google.com/file/d/1Zsdrh5r9EJ8LMlOhD_oM56P-dxrx6XOj/view?usp=drivesdk",
+      "Benedetta_Basic=Covenant": "https://drive.google.com/file/d/1S_J34JfRo6j5RDBTXfMe-5b3xjSMIJ6_/view?usp=drivesdk",
+      "Benedetta_Special=Covenant": "https://drive.google.com/file/d/11iHNvl92c_UZBOrYZshbYa8S3fLDL4W0/view?usp=drivesdk",
+      "Benedetta_Starlight=Covenant": "https://drive.google.com/file/d/1YeTuWEZfWvsOHUPn8SLKgyoQeovtn-gt/view?usp=drivesdk",
+      "Benedetta_MPL=Covenant": "https://drive.google.com/file/d/1LMJoMFFZAiWFbuRZqNZUeS0LZAjmih8M/view?usp=drivesdk",
+      
+      
+      //####################################################################################################################     
+      //####################################################################################################################
+      
+      //Cici Script Skin
+      
+      "Cici_Default=P.Ace": "https://drive.google.com/file/d/1M_-q9bPqmlDjj3oO7W3CcNMdhV33juzX/view?usp=drivesdk",
+      "Cici_Basic=P.Ace": "https://drive.google.com/file/d/1BERn8fXyk7J06yJkWjQdT0YC38spDn9e/view?usp=drivesdk",
+      "Cici_Starlight=P.Ace": "https://drive.google.com/file/d/1TnP7a8eq8fpHNsWyZbuXOL7gj1Zh8btD/view?usp=drivesdk",
+      
+      //####################################################################################################################
+      //####################################################################################################################
+     
+      //Chou Script Skin
+      
+      "Chou_Default=KOF": "https://drive.google.com/file/d/1zD43TNrxCIcuPc_6Vb_ErU_Ioj_DuCPK/view?usp=drivesdk", 
+      "Chou_Basic=KOF": "https://drive.google.com/file/d/1qKnnQDLoUCmVGQQBDCbZz-Ky6zYjeAza/view?usp=drivesdk",
+      "Chou_Elite=KOF": "https://drive.google.com/file/d/1pbOYrvMhwFKk4PV7sYS6bIJasA69aJcK/view?usp=drivesdk",
+      "Chou_Spacial=KOF": "https://drive.google.com/file/d/1ke9Tiv5hOz6aajLwIQ_ryqbgkpUzJRdA/view?usp=drivesdk",
+      
+      
+      //####################################################################################################################
+      //####################################################################################################################
+      //Granger_Legend
+      "Granger_Basic=Legend": "https://drive.google.com/file/d/1_TkPTv1ShwSCh63b_mK3u1jGSPSudnkL/view?usp=drivesdk",
+      "Granger_Default=Legend": "https://drive.google.com/file/d/1S7JBRHYq59fZctUaeq6gneY0HLPftvcy/view?usp=drivesdk",
+      "Granger_Lightborn=Legend": "https://drive.google.com/file/d/1qtimBCz5g2yf4BXwz1WF4BmUnRp3LuRq/view?usp=drivesdk",
+      "Granger_Starlight=Legend": "https://drive.google.com/file/d/1dQLGFiXliy2smKLvd7Ea3NHpkJlI8zjN/view?usp=drivesdk",
+      
+      //####################################################################################################################
+      //####################################################################################################################
+      
+      //Harith_Champion
+      "Harith_Default=Champion": "https://drive.google.com/file/d/1RilTCqBmvUseNfZp3Aorftdx2lJrGw2H/view?usp=drivesdk",
+      "Harith_Basic=Champion": "https://drive.google.com/file/d/1c9iNR0tq0Ak2s2FqC8EqraPyeFpxPMN_/view?usp=drivesdk",
+      "Harith_515=Champion": "https://drive.google.com/file/d/1l7y-8QjoYhkTWkHCTqqj0T0FBwDC60RM/view?usp=drivesdk",
+      "Harith_Lightborn=Champion": "https://drive.google.com/file/d/1Rwz-vpXGCuT9kIAztb6XOy_rLPab1Tor/view?usp=drivesdk",
+      "Harith_Christmas=Champion": "https://drive.google.com/file/d/1jWM4_JFIUFRIeYA_cKsDPm3xAjdSsvLY/view?usp=drivesdk",
+      //================================================================================================================================
+      
+      //Harith_Killua_HxH
+      "Harith_Default=Killua_HxH": "https://drive.google.com/file/d/1Mv3E8WrvIKn-knAkkjwGxhcy--0hVmCz/view?usp=drivesdk",
+      "Harith_Basic=Killua_HxH": "https://drive.google.com/file/d/1b2mUEsqDAgodNsRHPhw8ZhaftMxkv3Ff/view?usp=drivesdk",
+      "Harith_Elite=Killua_HxH": "https://drive.google.com/file/d/1wFEj3kTL69rIiKWywoJtr1jv_D6UqRxX/view?usp=drivesdk",
+      "Harith_515=Killua_HxH": "https://drive.google.com/file/d/1C7N772XdyTQsLjfOUejNaB2aiX7-tO21/view?usp=drivesdk",
+      "Harith_Lightborn=Killua_HxH": "https://drive.google.com/file/d/1iUrtXfaJy8PQ9X-dlDObRdikZ_TTgOrR/view?usp=drivesdk",
+      "Harith_Champions=Killua_HxH": "https://drive.google.com/file/d/1tdxzS1THOHFrpBbU0r_2GEHVs1T27M5p/view?usp=drivesdk",
+      "Harith_Christmas=Killua_HxH": "https://drive.google.com/file/d/19z6opZLVxL__44BAf0qUoxLamzwZDeun/view?usp=drivesdk",
+      "Harith_Collector=Killua_HxH": "https://drive.google.com/file/d/1qjuQj9neVTxStjgaTYf4GAAr-ntWsdr0/view?usp=drivesdk",
+      //####################################################################################################################
+      //####################################################################################################################
+      
+      //Lancelot
+      "Lancelot_Basic=Kishin_Densetsuy": "https://drive.google.com/file/d/1IolnNZw8HMELwxa68glDQY9gWsU2BIOE/view?usp=drivesdk",
+      "Lancelot_Default=Kishin_Densetsuy": "https://drive.google.com/file/d/1U_cFy8gqwFRe1wWqi3I60g4auppGU9Ay/view?usp=drivesdk",
+      "Lancelot_Elite=Kishin_Densetsuy": "https://drive.google.com/file/d/1qmfyVMVidYB7n7ab661T76nAcpRlbrpc/view?usp=drivesdk",
+      
+      //####################################################################################################################
+      //####################################################################################################################
+     
+      // Tambahkan lagi di sini sesuai postingan kamu....
+      
+    }
+    
+  }
+}
