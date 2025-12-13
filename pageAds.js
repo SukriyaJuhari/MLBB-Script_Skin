a61dc0d36bb827cbef11591d211f5d0ec3b1b99e
sukriyajuhari <sukriyajuhari@gmail.com>
2025-12-13 14:07:51 +0700

    update

--- pageAds.js
+++ pageAds.js
@@ -1,7 +1,15 @@
-setTimeout(() => {
 
-
-
+  
+  /*
+  Monetag pop-up agresif multi ads
+  let s = document.createElement("script");
+  s.src = "https://quge5.com/88/tag.min.js";
+  s.dataset.zone = "187166";
+  s.async = true;
+  s.setAttribute("data-cfasync", "false");
+  document.head.appendChild(s);
+  */
+  
   // Monetag — In-Page Push
   (function(s) {
     s.dataset.zone = '10219937';
@@ -12,26 +20,26 @@ setTimeout(() => {
   let s2 = document.createElement("script");
   s2.src = "//pl28110890.effectivegatecpm.com/76/21/02/7621028dfbf3b2a5f1351e92fc067d43.js";
   document.body.appendChild(s2);
-
+  
   // AdSense
   let g = document.createElement("script");
   g.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5527962581069268";
   g.async = true;
   g.crossorigin = "anonymous";
   document.head.appendChild(g);
-
-
-// Monetag — Interstitial / Vignette
+  
+  
+  // Monetag — Interstitial / Vignette
   (function(s) {
     s.dataset.zone = '10219839';
     s.src = 'https://groleegni.net/vignette.min.js';
   })(document.body.appendChild(document.createElement('script')));
-
-
+  
+  
   // Monetag — Vignette Banner
   (function(s) {
     s.dataset.zone = '10219940';
     s.src = 'https://gizokraijaw.net/vignette.min.js';
   })(document.body.appendChild(document.createElement('script')));
-
-}, 100);
+  
+  
